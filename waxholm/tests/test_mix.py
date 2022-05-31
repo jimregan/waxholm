238d1f4a6aa76ed8bbd3de80827c0c7ecf609e1c
Jim O'Regan <joregan@kth.se>
2022-05-31 17:54:26 +0200

    extend test

--- waxholm/tests/test_mix.py
+++ waxholm/tests/test_mix.py
@@ -3,4 +3,8 @@ from waxholm import FR
 def test_fr1():
     line = "FR       4481	 #sm	>pm #sm	>w XsmackX	 0.280 sec"
     fr = FR(line)
+    assert fr.type == "B"
+    assert fr.phone == "sm"
+    assert fr.frame == "4481"
+    assert fr.word == "XsmackX"
     print(fr)
