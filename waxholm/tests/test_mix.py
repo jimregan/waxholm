9c02d8cb03a9b73034e7001fa29be6db7211e16a
Jim O'Regan <joregan@kth.se>
2022-06-01 16:38:50 +0200

    also test inner and end line types

--- waxholm/tests/test_mix.py
+++ waxholm/tests/test_mix.py
@@ -2,7 +2,7 @@
 from waxholm import FR
 
 
-def test_fr1():
+def test_fr_begin1():
     line = "FR       4481	 #sm	>pm #sm	>w XsmackX	 0.280 sec"
     fr = FR(line)
     assert 'type' in fr.__dict__
@@ -23,7 +23,7 @@ def test_fr1():
     assert fr.seconds == "0.280"
 
 
-def test_fr2():
+def test_fr_begin2():
     line = "FR       6671	 #I	>pm #I	>w ikv{ll	 0.417 sec"
     fr = FR(line)
     assert 'type' in fr.__dict__
@@ -42,3 +42,39 @@ def test_fr2():
     assert fr.pm == "I"
     assert 'seconds' in fr.__dict__
     assert fr.seconds == "0.417"
+
+
+def test_fr_inner1():
+    line = "FR      10256	 $'[	>pm $'[	 0.641 sec"
+    fr = FR(line)
+    assert 'type' in fr.__dict__
+    assert fr.type == "I"
+    assert 'phone' in fr.__dict__
+    assert fr.phone == "'Ä"
+    assert 'frame' in fr.__dict__
+    assert fr.frame == "10256"
+    assert 'word' not in fr.__dict__
+    assert 'phone_type' in fr.__dict__
+    assert fr.phone_type == "$"
+    assert 'pm_type' in fr.__dict__
+    assert fr.pm_type == "$"
+    assert 'pm' in fr.__dict__
+    assert fr.pm == "'Ä"
+    assert 'seconds' in fr.__dict__
+    assert fr.seconds == "0.641"
+
+
+def test_fr_end1():
+    line = "FR      15241	 OK	 0.952 sec"
+    fr = FR(line)
+    assert 'type' in fr.__dict__
+    assert fr.type == "E"
+    assert 'phone' not in fr.__dict__
+    assert 'frame' in fr.__dict__
+    assert fr.frame == "15241"
+    assert 'word' not in fr.__dict__
+    assert 'phone_type' not in fr.__dict__
+    assert 'pm_type' not in fr.__dict__
+    assert 'pm' not in fr.__dict__
+    assert 'seconds' in fr.__dict__
+    assert fr.seconds == "0.952"
