cb56f515bc5f3721a9fc22a12dec8ae771255774
Jim O'Regan <joregan@kth.se>
2022-09-06 13:58:24 +0200

    prune

--- scripts/convert_to_textgrid.py
+++ scripts/convert_to_textgrid.py
@@ -11,40 +11,6 @@ from pathlib import Path
 from waxholm.audio import smp_to_wav
 
 
-Label = namedtuple('Label', ['start', 'end', 'label'])
-
-def get_label_tuples(mix):
-    times = mix.get_phone_label_tuples()
-    return [Label(start = x[0], end = x[1], label = x[2]) for x in times]
-
-
-def prune_empty_labels(labels, debug = False):
-    out = []
-    for label in labels:
-        if label[0] != label[1]:
-            out.append(label)
-        else:
-            if debug:
-                print(f"Start: ({label[0]}); end: ({label[1]}); label {label[2]}")    
-    return out
-
-
-def get_merged_phone_intervals(mix):
-    return mix.merge_plosives()
-
-
-def get_word_intervals(mix):
-    times = mix.get_time_pairs()
-    if len(times) == len(mix.fr[0:-1]):
-        out = []
-        for z in zip(times, mix.fr[0:-1]):
-            if z[1].type == "B":
-                out.append(Interval(z[0][0], z[0][1], z[1].word))
-        return out
-    else:
-        return []
-
-
 def main():
     parser = argparse.ArgumentParser(description='Convert .mix to Praat textgrid.')
     parser.add_argument('files', type=str, nargs='+', help='files to process')
@@ -83,8 +49,8 @@ def main():
         mix = Mix(file)
         tg = textgrid.Textgrid()
 
-        word_tier = textgrid.IntervalTier("words", get_word_intervals(mix))
-        phone_tier = textgrid.IntervalTier("phones", get_merged_phone_intervals(mix))
+        word_tier = textgrid.IntervalTier("words", mix.get_word_label_tuples())
+        phone_tier = textgrid.IntervalTier("phones", mix.get_merged_plosives())
 
         tg.addTier(word_tier, reportingMode="error")
         tg.addTier(phone_tier, reportingMode="error")
